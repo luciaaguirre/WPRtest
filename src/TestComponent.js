c0fbf4edd5ffdc6a08ab99283120982b1cb9a658
Lucia Aguirre <guerrolucia@gmail.com>
2025-03-27 17:21:16 +0100

    adds test component

---
+++ src/TestComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TestComponent() {
+    return (
+        <div className="test">
+        <h1> Hello</h1>
+        <p>Hello Again</p>
+        </div>
+    )
+}
+
+export default TestComponent
